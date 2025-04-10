6ed4b0073386b8d5c50d526da336620df3e550f0
hpsinf <hps.inf@gmail.com>
2025-04-10 15:15:26 -0300

    adicão de record

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,7 +1,11 @@
 package br.com.alura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -12,6 +16,12 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo (String nome, int anoDeLancamento){
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TitulosOmbd titulosOmbd) {
+        this.nome = titulosOmbd.title();
+        this.anoDeLancamento = Integer.valueOf(titulosOmbd.year().substring(0,4).trim());
+        this.duracaoEmMinutos = Integer.valueOf(titulosOmbd.runtime().substring(0,2).trim());
     }
 
     public String getNome() {
@@ -67,5 +77,14 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo tituloAComparar) {
         return this.getNome().compareTo(tituloAComparar.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "Titulo :" +
+                "nome :'" + nome + '\'' +
+                ", anoDeLancamento: " + anoDeLancamento +
+                ", duracaoEmMinutos: " + duracaoEmMinutos +
+                ' ';
     }
 }
--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,11 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TitulosOmbd;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,17 +15,29 @@ import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Informe o nome do filme para a busca: ");
-        var busca = scanner.nextLine();
-        String enderecoBusca = "http://www.omdbapi.com/?t="+busca+"&apikey=ad8d0888";
-        try (HttpClient client = HttpClient.newHttpClient()) {
+        try (Scanner scanner = new Scanner(System.in);
+             HttpClient client = HttpClient.newHttpClient()) {
+
+            System.out.println("Informe o nome do filme para a busca: ");
+            var busca = scanner.nextLine();
+            String enderecoBusca = "http://www.omdbapi.com/?t=" + busca + "&apikey=ad8d0888";
+
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(enderecoBusca))
                     .build();
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
+            var respJson = response.body();
+            System.out.println(respJson);
+
+            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+            TitulosOmbd respTituloOmdb = gson.fromJson(respJson, TitulosOmbd.class);
+
+            Titulo meutitulo = new Titulo(respTituloOmdb);
+
+            System.out.println(meutitulo);
+
+
         }
     }
 }
